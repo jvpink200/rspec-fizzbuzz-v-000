c28be73fc624d0a29f022ff85093cace54363dd6
jvpink200 <jvillarr20@gmail.com>
2018-09-08 03:19:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if number % 3 === 0  # if the number is divisible by 3 then modulous/remainder should be 0
+  if number % 3 == 0  # if the number is divisible by 3 then modulous/remainder should be 0
     puts "Fizz"
-  elsif number % 5 === 0
+  elsif number % 5 == 0
     puts "Buzz"
-  elsif number % 3 === 0 && number % 5 === 0
+  elsif number % 3 == 0 && number % 5 == 0
   puts "FizzBuzz"
   else
     puts "nil"
